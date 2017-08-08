78324fe8944816e86bccdf1bfd54516baad7c8f0
Florian PONROY - OpenClassrooms <florian.ponroy@openclassrooms.com>
2017-08-08 19:05:53 +0200

    Created the variables for the three widgets, and referenced them in the code.

--- app/src/main/java/com/ponroy/florian/topquiz/MainActivity.java
+++ app/src/main/java/com/ponroy/florian/topquiz/MainActivity.java
@@ -2,12 +2,24 @@ package com.ponroy.florian.topquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mGreetingText;
+    private EditText mNameInput;
+    private Button mPlayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
+        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
+        mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
     }
 }
