a3937dfa9e043226eb6f3aa65342445f63159c6f
Florian PONROY - OpenClassrooms <florian.ponroy@openclassrooms.com>
2017-08-08 19:06:38 +0200

    Created listeners for the EditText and the Button. Disabled the button as long as the user did not type a character.

--- app/src/main/java/com/ponroy/florian/topquiz/MainActivity.java
+++ app/src/main/java/com/ponroy/florian/topquiz/MainActivity.java
@@ -2,6 +2,8 @@ package com.ponroy.florian.topquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,5 +23,31 @@ public class MainActivity extends AppCompatActivity {
         mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
         mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
         mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
+
+        mPlayButton.setEnabled(false);
+
+        mNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPlayButton.setEnabled(s.toString().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // User clicked the button
+            }
+        });
     }
 }
