4de3fbb0171f0a670735879b1fdbc75f0ab60973
Florian PONROY - OpenClassrooms <florian.ponroy@openclassrooms.com>
2017-08-09 10:26:23 +0200

    Wired the widgets in the Game Activity

--- app/src/main/java/com/ponroy/florian/topquiz/controller/GameActivity.java
+++ app/src/main/java/com/ponroy/florian/topquiz/controller/GameActivity.java
@@ -2,14 +2,35 @@ package com.ponroy.florian.topquiz.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.ponroy.florian.topquiz.R;
 
 public class GameActivity extends AppCompatActivity {
 
+    private TextView mQuestionTextView;
+    private Button mAnswerButton1;
+    private Button mAnswerButton2;
+    private Button mAnswerButton3;
+    private Button mAnswerButton4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        // Wire widgets
+        mQuestionTextView = (TextView) findViewById(R.id.activity_game_question_text);
+        mAnswerButton1 = (Button) findViewById(R.id.activity_game_answer1_btn);
+        mAnswerButton2 = (Button) findViewById(R.id.activity_game_answer2_btn);
+        mAnswerButton3 = (Button) findViewById(R.id.activity_game_answer3_btn);
+        mAnswerButton4 = (Button) findViewById(R.id.activity_game_answer4_btn);
+
+        // Use the tag property to 'name' the buttons
+        mAnswerButton1.setTag(0);
+        mAnswerButton2.setTag(1);
+        mAnswerButton3.setTag(2);
+        mAnswerButton4.setTag(3);
     }
 }
