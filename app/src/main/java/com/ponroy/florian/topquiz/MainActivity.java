6f8e1fb2ecee55e75b4bd8bf4f2b336d2d1669a6
Florian PONROY - OpenClassrooms <florian.ponroy@openclassrooms.com>
2017-08-09 09:34:47 +0200

    Started the GameActivity when the user clicks the Play button

--- app/src/main/java/com/ponroy/florian/topquiz/MainActivity.java
+++ app/src/main/java/com/ponroy/florian/topquiz/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ponroy.florian.topquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -47,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // User clicked the button
+                Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(gameActivityIntent);
             }
         });
     }
