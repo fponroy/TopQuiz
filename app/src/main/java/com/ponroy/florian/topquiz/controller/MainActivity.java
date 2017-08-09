c9b32580691dad723a8f232f795a6ae5bdf045e2
Florian PONROY - OpenClassrooms <florian.ponroy@openclassrooms.com>
2017-08-09 10:43:44 +0200

    Fixed an issue with the User model: instance was not created in the Main Activity.

--- app/src/main/java/com/ponroy/florian/topquiz/controller/MainActivity.java
+++ app/src/main/java/com/ponroy/florian/topquiz/controller/MainActivity.java
@@ -25,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mUser = new User();
+
         mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
         mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
         mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
